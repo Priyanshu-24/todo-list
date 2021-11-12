f07fda334ce75683c80fcc1f309a2403d3f039d6
Priyanshu Pandey <priyanshu24pandey@gmail.com>
2021-11-12 10:20:25 +0530

    Notes logic done

--- js/app.js
+++ js/app.js
@@ -9,9 +9,11 @@ const note_display = document.getElementById("add-note");
 
 const addTodo = document.getElementById("add-todo-btn");
 const addProject = document.getElementById("add-project-btn");
+const addNote = document.getElementById("add-note-btn");
 
-let todo = [];
+let todo =    [];
 let project = [];
+let note =    [];
 
 project_display.style.display = "none";
 note_display.style.display = "none";
@@ -38,6 +40,8 @@ project_tab.addEventListener("click", function () {
 });
 
 note_tab.addEventListener("click", function () {
+
+  displayNote();
 
   todo_display.style.display = "none";
   note_display.style.display = "flex";
@@ -106,6 +110,34 @@ addProject.addEventListener("click", function () {
   }
 });
 
+
+addNote.addEventListener("click", function () {
+
+  let note_des = document.getElementById("add-note-des");
+
+  console.log(note_des.value);
+
+  if (note_des.value == "")
+    alert("Required Input Field is missing");
+
+  else {
+    let notes = JSON.parse(localStorage.getItem("note"));
+
+    if (notes) note = notes;
+
+    note.push({
+      note_des: note_des.value,
+    });
+    
+    localStorage.setItem("note", JSON.stringify(note));
+
+    displayNote()
+
+    note_des.value = "";
+
+  }
+});
+
 function displayTodo() {
   let todos = JSON.parse(localStorage.getItem("todo"));
 
@@ -150,6 +182,28 @@ function displayProject() {
   }
 }
 
+
+function displayNote() {
+  let notes = JSON.parse(localStorage.getItem("note"));
+
+  if (notes !== null) {
+    let html = "";
+
+    notes.forEach(function (element, index) {
+      html += `
+        <div class = "project-card">
+    <div>${element.note_des}</div><br>
+    <div><button id = "${index}" onClick = "deleteNote(this.id)" class = "dlt-btn-project">Delete</button></div>
+    </div>
+        `;
+    });
+
+    let a = document.getElementById("display-note");
+    a.innerHTML = html;
+  }
+}
+
+
 function deleteTodo(index) {
   let todos = JSON.parse(localStorage.getItem("todo"));
   todos.splice(index, 1);
@@ -162,6 +216,14 @@ function deleteProject(index) {
     projects.splice(index, 1);
     localStorage.setItem("project", JSON.stringify(projects));
     displayProject();
+  }
+
+
+  function deleteNote(index) {
+    let notes = JSON.parse(localStorage.getItem("note"));
+    notes.splice(index, 1);
+    localStorage.setItem("note", JSON.stringify(notes));
+    displayNote();
   }
 
 
